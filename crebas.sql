b9f549b5e6d21442816511e0bc6790e206bf220e
MTAREK19 <moubariki.tarik@gmail.com>
2020-05-07 14:00:32 +0000

    fr

--- crebas.sql
+++ crebas.sql
@@ -1,16 +1,6 @@
-/*==============================================================*/
-/* Nom de SGBD :  MySQL 5.0                                     */
-/* Date de création :  28/04/2020 22:17:28                      */
-/*==============================================================*/
-CREATE DATABASE newtm;
-USE newtm;
+CREATE database youcode;
+USE youcode;
 
-
-drop table if exists Client;
-
-drop table if exists Reservation;
-
-drop table if exists Voiture;
 
 /*==============================================================*/
 /* Table : Client                                               */
@@ -19,7 +9,7 @@ create table Client
 (
    idClient             int not null,
    nom                  varchar(254),
-   email                varchar(254),
+   email               varchar(254),
    password             varchar(254),
    primary key (idClient)
 );
@@ -27,12 +17,13 @@ create table Client
 /*==============================================================*/
 /* Table : Reservation                                          */
 /*==============================================================*/
-create table Reservation
-(
-   idClient             int,
-   idVoiture            int,
-   dateDebut            datetime,
-   dateRetour           datetime
+create table Reservation (
+   idClient  int,
+   idVoiteur int, 
+   dateDebut            datetime             null,
+   dateRetour           datetime             null,
+   PRIMARY KEY (idClient)
+
 );
 
 /*==============================================================*/
@@ -46,89 +37,26 @@ create table Voiture
    primary key (idVoiture)
 );
 
-alter table Reservation add constraint FK_association2 foreign key (idVoiture)
-      references Voiture (idVoiture) on delete restrict on update restrict;
+select * from Client;
+select * from  Reservation;
+select * from Voiture;
 
-alter table Reservation add constraint FK_association3 foreign key (idClient)
-      references Client (idClient) on delete restrict on update restrict;
+INSERT INTO Client values (1,"tarek","tarek@youcode.com","azerty");
+INSERT INTO Client values (2,"abdo","abdo@youcode.com","qwerty");
+
+INSERT INTO Voiture values (1,"BMW.PNG","BMW");
+INSERT INTO Voiture values (1,"DACIA.PNG","DACIA");
+
+INSERT INTO Reservation values (1,1,"20_05_15","20_05_18");
+INSERT INTO Reservation values (2,2,"20_05_17","20_05_19");
+
+UPDATE Client SET password="qsdfgh" where idClient=1;
+UPDATE Reservation SET dateRetour="12_05_20" where idClient=1;
+UPDATE Voiture SET image="BMNOUVEAU" where idVoiture=1;
+
+DELETE FROM Client WHERE idClient='2';
 
 
-/*==============================================================*/
-/* Inset data in Voiture Table                                           */
-/*==============================================================*/
-insert into Voiture
-values (1,"img1","BMW");
-insert into Voiture
-values (2,"img2","DACIA");
-/*==============================================================*/
-/* Inset data in Reservation Table                                           */
-/*==============================================================*/
-insert into Reservation
-values (1,1,'2020-04-30','2020-04-26');
-insert into Reservation
-values (2,2,'2020-05-12','2020-08-26');
-
-/*==============================================================*/
-/* Inset data in Client Table                                           */
-/*==============================================================*/
-INSERT INTO Client
-VALUES (1, 'fna', 'fna@hotmail', 'azerty');
-INSERT INTO Client
-VALUES (2, 'ycd', 'ycd@hotmail', 'hjhjgjg');
-
-/*==============================================================*/
-/* Update in Voiture Table                                           */
-/*==============================================================*/
-update Voiture
-set marque = 'polo'
-where id = 2;
-
-/*==============================================================*/
-/* Delate in voiture Table                                           */
-/*==============================================================*/
-delete from Voiture where id = 2;
-
--- show table
- select * from Voiture;
-
-/*==============================================================*/
-/* Update in Reservation Table                                           */
-/*==============================================================*/
-update Reservation
-set dateRetour = '2020-04-17'
-where id = 2;
-
-/*==============================================================*/
-/* Delate inReservation Table                                           */
-/*==============================================================*/
-delete from Reservation where id = 2;
-
--- show table
- select * from Reservation;
-/*==============================================================*/
-/* Update Client Table                                           */
-/*==============================================================*/
-UPDATE client
-SET email='Juan@hotmail'
-WHERE idClient =2;
-
-/*==============================================================*/
-/* Delete in Client Table                                           */
-/*==============================================================*/
-DELETE FROM Client WHERE idClient='1';
-select * from newtm.client;
-/*==============================================================*/
-/* Create user +  permission                                  */
-/*==============================================================*/
 CREATE USER 'tarek'@'localhost' IDENTIFIED BY 'azerty';
-
- grant insert on newtm.* to 'tarek'@'localhost';
-
----------------------------------------------------------------------
-
-CREATE USER 'moubariki'@'localhost' IDENTIFIED BY 'qwerty';
-
- grant ALL PRIVILEGES on newtm.* to 'moubariki'@'localhost';
- /*==============================================================*/
-
+grant insert on youcode.* to 'tarek'@'localhost';
 
